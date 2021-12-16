1cffc023560278acb1a4dca71d99fac69b875dc2
Chantal <chantalbrousseau@rogers.com>
2021-12-16 03:11:20 -0500

    dep test

--- src/pages/_reset.svelte
+++ src/pages/_reset.svelte
@@ -35,14 +35,14 @@
   * :global(*) {
     text-align: center;
   }
-  a {
+  /* a {
     position: fixed;
     top: 0;
     left: 0;
     padding: 8px 16px;
     background: #555;
     color: white;
-  }
+  } */
 </style>
 
 <div style="height: 100%">
